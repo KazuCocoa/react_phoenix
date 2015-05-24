141ed5c668c2ea6f280b77126bf35c6d69c213be
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-05-25 00:38:39 +0900

    update react

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -8,25 +8,66 @@ import {Socket} from "phoenix"
 $(function() {
   var App = React.createClass( {
     getInitialState: function() {
-      return { message: "" }
+      return { 
+        message: "",
+        savedMessages: []
+      };
     },
+
     updateMessage: function(e) {
       this.setState({ message: e.target.value });
+    },
+
+    saveMessage: function(message) {
+      var messages = this.state.savedMessages.concat(message);
+      this.setState({ savedMessages: messages});
     },
 
     render: function() {
       return (
         <div>
-          <input type="text" onChange={this.updateMessage} />
-          <Message message={this.state.message} />
+          <MessageInput onChange={this.updateMessage}
+                        onSave={this.saveMessage} />
+          <Message message={this.state.message}
+              savedMessages={this.state.savedMessages} />
         </div>
       );
     }
   });
 
+  var MessageInput = React.createClass({
+    _onChange: function(e) {
+      this.props.onChange(e.target.value);
+    },
+
+    _onKeyDown: function(e) {
+      //13 == Enter Key Code
+      if (e.keyCode === 13) {
+        this.props.onSave(e.target.value);
+        e.target.value = "";
+      }
+    },
+
+    render: function() {
+      return <input type="text" onChange={this._onChange}
+                                onKeyDown={this._onKeyDown} />;
+    }
+  });
+
   var Message = React.createClass({
     render: function() {
-      return <p>{this.props.message}</p>;
+      var messages = this.props.savedMessages.map(
+        function (message) {
+          return <li>{message}</li>;
+        }
+      );
+
+      return (
+        <div>
+          <p>{this.props.message}</p>
+          <ul>{messages}</ul>
+        </div>
+      )
     }
   });
 
@@ -39,7 +80,6 @@ $(function() {
     <App />,
     document.getElementById('app-container')
   );
-
 });
 
 let App = {
