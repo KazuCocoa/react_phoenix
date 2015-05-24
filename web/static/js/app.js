40059a67bac3d3383e1f0aa67834d049a3fb591f
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-05-24 23:16:45 +0900

    add simple input text of web+db

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -6,10 +6,33 @@ import {Socket} from "phoenix"
 // })
 
 $(function() {
+  var App = React.createClass( {
+    getInitialState: function () {
+      return { message: "" }
+    },
+    updateMessage: function(e) {
+      this.setState({ message: e.target.value });
+    },
+    render: function() {
+      return(
+        <div>
+          <input type="text" onChange={this.updateMessage} />
+          <p>{this.state.message}</p>
+        </div>
+      );
+    }
+  });
+
   React.render(
     <h2 className="jumbotron">Hello from React!</h2>,
-    document.getElementById('hello_world')
+    document.getElementById('hello-world')
   );
+
+  React.render(
+    <App />,
+    document.getElementById('app-container')
+  );
+
 });
 
 let App = {
